8964ea1937742d474f1d2e095ca82288286ac00c
joseluis47 <jluiss@bol.com.br>
2021-02-15 12:25:41 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+
+require_once("vendor/autoload.php");
+
+$app = new \Slim\Slim();
+
+$app->config('debug', true);
+
+$app->get('/', function() {
+    
+	$sql = new Hcode\DB\sql();
+	$result= $sql->select("select *from tb_users");
+	echo json_encode($result);
+
+});
+
+$app->run();
+
+ ?>
